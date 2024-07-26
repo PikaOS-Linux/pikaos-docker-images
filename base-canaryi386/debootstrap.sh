8e0da4ea76082154b6325023e8d9187927f45711
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-26 16:01:55 +0300

    canaryi386 base

--- base-canaryi386/debootstrap.sh
+++ base-canaryi386/debootstrap.sh
@@ -4,4 +4,4 @@ DIST=sid ARCH=i386 debootstrap --arch=i386 sid base_chroot || true
 rm -rf base_chroot/debootstrap
 #chroot ./base_chroot /bin/dash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 cd  ./base_chroot
-sudo tar -czvf ../base_chroot.tgz ./*
+tar -czvf ../base_chroot.tgz ./*
