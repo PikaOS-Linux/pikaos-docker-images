f9a6bafbec685b7d5ce92b9f3e24f04bfa2340cf
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-26 15:52:16 +0300

    canaryi386 base

--- base-canaryi386/debootstrap.sh
+++ base-canaryi386/debootstrap.sh
@@ -2,8 +2,6 @@
 set -e
 DIST=sid ARCH=i386 debootstrap --arch=i386 sid base_chroot || true
 rm -rf base_chroot/debootstrap
-chroot ./base_chroot /bin/dash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
-chroot ./base_chroot /bin/dash -c "DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y"
-chroot ./base_chroot /bin/dash -c "ln -sfv /bin/dash /bin/sh"
+#chroot ./base_chroot /bin/dash -c "DEBIAN_FRONTEND=noninteractive apt-get -y update"
 cd  ./base_chroot
 sudo tar -czvf ../base_chroot.tgz ./*
